bb23141f4e5a5295dfc7f31e20c0463b77c60a64
zhangxuqiang <zhangxuqiang@antsanimation.com>
2016-12-07 16:47:19 +0800

    Fix:need reload

--- Publish/Publish_CFX_Hide_CFXGroup.py
+++ Publish/Publish_CFX_Hide_CFXGroup.py
@@ -3,4 +3,5 @@ import Publish_CFX_Show_CFXGroup as pcsc
 
 
 def run():
+    reload(pcsc)
     pcsc.config_cfx_group(0)
--- Publish/Publish_CFX_Hide_CLoth.py
+++ Publish/Publish_CFX_Hide_CLoth.py
@@ -2,4 +2,5 @@ import Publish_CFX_Export_CFX as pce
 
 
 def run():
+    reload(pce)
     pce.configCloth(0)
