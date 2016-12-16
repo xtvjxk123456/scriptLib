1b9770fcf990827ed31b963a4733ec657dedd733
zhangxuqiang <zhangxuqiang@antsanimation.com>
2016-12-16 16:34:13 +0800

    Add:update code in CamCache

--- AbcCache/AbcCache_CamCache_CustomCamCache.py
+++ AbcCache/AbcCache_CamCache_CustomCamCache.py
@@ -1,16 +1,33 @@
-import pixoLibs.pixoFileTools as pft 
-import maya.mel as mel
+#coding:utf-8
+import pixoLibs.pixoFileTools as pft
 import maya.cmds as mc
+import maya.mel as mel
 import os
+import Publish_Layout_Bake_CameraToWorld as plb
 
-#change here
+# change here
 _cacheDir = r'Z:\Shotgun\projects\df\data\CamAbc'
-def run():                                                                   
-    path_obj = pft.PathDetails.parse_path( mc.file(q=1,sn=1))
-    startTime = int(mc.playbackOptions(ast=True, q=True))
-    endTime =   int(mc.playbackOptions(aet=True, q=True))
-    cam_PublishFullPath_abc =_cacheDir+'\{}\{}'.format(path_obj.seq,path_obj.shot)+'\{}'.format(path_obj.task)+ '\\'+mc.ls(sl=True)[0]+'.abc'
-    # mel.eval('AbcExport -j "-frameRange '+str(startTime)+' '+str(endTime)+' -stripNamespaces -worldSpace -root '+ mc.ls(sl=True,l=True)[0] +' -file '+cam_PublishFullPath_abc+'"')
-    mc.sysFile(os.path.dirname(cam_PublishFullPath_abc),md=True)
-    mel.eval('AbcExport -j "-frameRange {} {} -stripNamespaces -worldSpace -root {} -file {}"'.format(str(startTime),str(endTime),mc.ls(sl=True,l=True)[0],os.path.normpath(cam_PublishFullPath_abc).replace('\\','/')))
 
+
+def run():
+    path_obj = pft.PathDetails.parse_path(mc.file(q=1, sn=1))
+    startTime = int(mc.playbackOptions(ast=True, q=True))
+    endTime = int(mc.playbackOptions(aet=True, q=True))
+    cam_PublishFullPath_abc = _cacheDir + '\{}\{}'.format(path_obj.seq, path_obj.shot) + '\{}'.format(
+        path_obj.task) + '\\' + 'cam_{}_{}.abc'.format(path_obj.seq, path_obj.shot)
+    mc.sysFile(os.path.dirname(cam_PublishFullPath_abc), md=True)
+    cam = plb.getCameraInShot()
+    if len(cam) == 1:
+        mel.eval(
+            'AbcExport -j "-frameRange {} {} -stripNamespaces -worldSpace -root {} -file {}"'.format(str(startTime),
+                                                                                                     str(endTime),
+                                                                                                     mc.ls(
+                                                                                                         mc.listRelatives(
+                                                                                                             cam[0],
+                                                                                                             p=True),
+                                                                                                         l=True)[0],
+                                                                                                     os.path.normpath(
+                                                                                                         cam_PublishFullPath_abc).replace(
+                                                                                                         '\\', '/')))
+    else:
+        mc.warning(u'请检查场景相机,有问题存在')
