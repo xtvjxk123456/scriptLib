e6a450d669bc192172e7f6fc6fed4ac99fe8cf4a
zhangxuqiang <zhangxuqiang@antsanimation.com>
2016-12-19 20:01:51 +0800

    fix:切换代理的代码整理

--- Lighting/Lighting_Env_ReplaceSelectedProxyWithRef.py
+++ Lighting/Lighting_Env_ReplaceSelectedProxyWithRef.py
@@ -1,10 +1,42 @@
 # coding:utf-8
 import pixoMaya.td_tools as td_tools
 import pymel.core as pm
+import pixoLibs.pixoFileTools as pft
+
+
+def get_shader_file_for_selected_proxy(obj):
+    sel = obj
+    sel_shape = td_tools.return_shape(sel)
+    if isinstance(sel_shape, pm.nodetypes.AiStandIn):
+        print 'valid'
+        arnold_proxy_path = pm.getAttr('%s.dso' % sel_shape)
+        dets = pft.PathDetails.parse_path(arnold_proxy_path)
+        dets.ext = 'm*'
+        dets.name = None
+        dets.user = '*'
+        files = list(dets.findMatchingFiles())
+        return files[0].getPublishFullPath()
+    else:
+        print 'not a valid arnold proxy'
+        return None
 
 
 def run():
     if pm.ls(sl=True):
-        td_tools.replace_selected_proxy_with_shader_file()
+        for x in pm.ls(sl=True):
+
+            layers = td_tools.which_render_layers(x)
+            shader_file = get_shader_file_for_selected_proxy(x)
+            pm.delete(x)
+            dets = pft.PathDetails.parse_path(shader_file)
+            if dets.category == 'assets':
+                namespace = dets.shot
+            else:
+                if dets.task == 'anim':
+                    namespace = dets.name
+            # if the namespace exists - remove it
+            td_tools.removeNamespace(namespace)
+            pm.createReference(shader_file, namespace=namespace)
+            td_tools.set_render_layers('%s:%s_mdl' % (namespace, namespace), layers)
     else:
         pm.warning(u'请先选中需要替换的代理内容')
--- Lighting/Lighting_Env_ReplaceSeletedRefWithProxy.py
+++ Lighting/Lighting_Env_ReplaceSeletedRefWithProxy.py
@@ -1,10 +1,48 @@
 # coding:utf-8
 import pixoMaya.td_tools as td_tools
 import pymel.core as pm
+import pixoLibs.pixoFileTools as pft
+
+
+def get_proxy_for_selected_reference(obj):
+    ref_obj = td_tools.ReferenceObj(obj)
+    filepath = ref_obj.file
+    if filepath:
+        if '{' in filepath:
+            filepath = filepath.split('{')[0]
+        dets = pft.PathDetails.parse_path(filepath)
+        dets.user = '*'
+        dets.ext = '*'
+        dets.name = 'proxy'
+        files = list(dets.findMatchingFiles())
+        return files[0].getPublishFullPath()
+    else:
+        return None
 
 
 def run():
     if pm.ls(sl=True):
-        td_tools.replace_selected_ref_with_proxy()
+        for x in pm.ls(sl=True):
+            layers = td_tools.which_render_layers(x)
+            ref_obj = td_tools.ReferenceObj(x)
+            print layers
+            ref = pm.FileReference(ref_obj.file)
+            proxy_file = get_proxy_for_selected_reference(x)
+            ref.remove()
+            # import proxy
+            dets = pft.PathDetails.parse_path(proxy_file)
+            name_space = dets.shot
+            try:
+                pm.system.namespace(add=name_space)
+            except RuntimeError:
+                pass
+            pm.system.namespace(set=name_space)
+            pm.importFile(proxy_file)
+            pm.system.namespace(set=':')
+            sel = pm.PyNode('%s:ArnoldStandIn' % name_space)
+            sel.rename('%s:%s_mdl' % (name_space, name_space))
+            # add this to all the render layers.
+            print 'trying to add this:  %s:%s_mdl' % (name_space, name_space)
+            td_tools.set_render_layers('%s:%s_mdl' % (name_space, name_space), layers)
     else:
         pm.warning(u'请先选中需要替换的参考内容')
