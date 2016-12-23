2f4b98a9da5bb08adad65ffbbf312eb1ded362a1
zhangxuqiang <zhangxuqiang@antsanimation.com>
2016-12-23 15:45:07 +0800

    Fix:Cam_publish脚本进行了些修复

--- Publish/Publish_Cam_CamPublish.py
+++ Publish/Publish_Cam_CamPublish.py
@@ -1,5 +1,15 @@
-import pixoMaya.shelf.publish_cam as cam_publish
+# coding:utf-8
 
 
 def run():
+    import Publish_Cam_Fix_CameraName
+    Publish_Cam_Fix_CameraName.run()
+
+    import Publish_Layout_Lock_Camrea
+    Publish_Layout_Lock_Camrea.run()
+
+    import Publish_Cam_Delete_UselessNodeOnCamera
+    Publish_Cam_Delete_UselessNodeOnCamera.run()
+
+    import pixoMaya.shelf.publish_cam as cam_publish
     cam_publish.run()
