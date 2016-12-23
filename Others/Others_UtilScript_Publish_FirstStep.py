a1c605efabba05684c3edcbb7492c419ff06c49b
zhangxuqiang <zhangxuqiang@antsanimation.com>
2016-12-23 15:57:17 +0800

    Fix:first step脚本进行修复

--- Others/Others_UtilScript_Publish_FirstStep.py
+++ Others/Others_UtilScript_Publish_FirstStep.py
@@ -5,15 +5,6 @@ def run():
     import pixoMaya.shelf.playblast as pb
     pb.run()
     # ------------------------------------------
-    import Publish_Cam_Fix_CameraName
-    Publish_Cam_Fix_CameraName.run()
-
-    import Publish_Layout_Lock_Camrea
-    Publish_Layout_Lock_Camrea.run()
-
-    import Publish_Cam_Delete_UselessNodeOnCamera
-    Publish_Cam_Delete_UselessNodeOnCamera.run()
-
     import Publish_Cam_CamPublish
     Publish_Cam_CamPublish.run()
     # ------------------------------------------
