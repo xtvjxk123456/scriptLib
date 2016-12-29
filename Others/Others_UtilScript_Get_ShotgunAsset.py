02e3a5e047c3a96fb97225a63a012b1642b796d8
zhangxuqiang <zhangxuqiang@antsanimation.com>
2016-12-29 15:58:35 +0800

    Fix:finish task code

--- Others/Others_UtilScript_Get_ShotgunAsset.py
+++ Others/Others_UtilScript_Get_ShotgunAsset.py
@@ -12,7 +12,7 @@ def getShotInfo(shot):
         ['project', 'is', {'type': 'Project', 'id': projeceInfo['id']}]
     ],
                            ['assets', 'sg_asset_type', 'sg_head_in', 'sg_tail_out', 'sg_cut_in', 'sg_cut_out',
-                            'sg_resolution', 'code','tasks'])
+                            'sg_resolution', 'code', 'tasks'])
     return shotInfo
 
 
@@ -26,12 +26,23 @@ def getAssetType(asset):
     return assetInfo
 
 
-def run():
+def getTaskInfor(taskName):
+    projeceInfo = sg.find_one("Project", [['name', 'is', 'df']], ['code', 'sg_description', 'project', 'name'])
+    taskInfor = sg.find_one("Task", [
+        ['content', 'is', taskName],
+        ['project', 'is', {'type': 'Project', 'id': projeceInfo['id']}]
+    ],
+                            ['sg_task_type', 'code', 'task_assignees'])
 
+    return taskInfor
+
+
+def run():
     current_shot = mc.file(q=True, sn=True).split('_')
     shot_name = '_'.join(current_shot[1:3])
 
     assets = getShotInfo(shot_name)['assets']
+    tasks = getShotInfo(shot_name)['tasks']
     print '#' * 60
     print '-------------shot %s----------------' % shot_name
     print 'FrameRange is {}-{}'.format(getShotInfo(shot_name)['sg_cut_in'], getShotInfo(shot_name)['sg_cut_out'])
@@ -39,8 +50,14 @@ def run():
     print 'asset content:'
     for x in assets:
         print '-- [', x['name'], ']  AssetType : <', getAssetType(x['name'])['sg_asset_type'], '>'
+    print 'task content:'
+    for x in tasks:
+        assignto = getTaskInfor(x['name'])['task_assignees']
+        if assignto:
+            user_name = assignto[0]['name'].decode('utf-8')
+        else:
+            user_name = None
+
+        print '-- [', x['name'], ']  AssignTo : <', user_name, '>'
     print '#' * 60
-
-
-
-
+    mc.ScriptEditor()
