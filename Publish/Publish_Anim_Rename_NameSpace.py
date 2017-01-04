cc172593672b9f8644d1621e851636fddb97f10a
zhangxuqiang <xtvjxk123455maya@163.com>
2017-01-05 01:25:05 +0800

    Fix:change asset name get method

--- Publish/Publish_Anim_Rename_NameSpace.py
+++ Publish/Publish_Anim_Rename_NameSpace.py
@@ -1,30 +1,31 @@
+# coding:utf-8
 import pymel.core as pm
-import re,os
+import re, os
 import maya.cmds as mc
 
 
 def run():
     references = pm.ls(type='reference')
 
-    if len(references)== 0:
+    if len(references) == 0:
         mc.warning('no References!!!!!!!')
         return None
     for x in references:
         try:
-            pm.referenceQuery(x,f=True,wcn=True)
+            pm.referenceQuery(x, f=True, wcn=True)
         except Exception:
             continue
-        ref= pm.FileReference(x) 
+        ref = pm.FileReference(x)
         normalPath = ref.path
-        assetName= re.match(r'[a-zA-Z]+_([a-zA-Z]+)_[a-zA-Z]+_[a-zA-Z]+[\d]+_*',os.path.basename(normalPath)).group(1)
+        assetName = os.path.basename(normalPath).split('_')[1]
         cnPath = ref.withCopyNumber()
 
         if normalPath != cnPath:
-            num= re.match(r'.*?\{(\d*)\}',cnPath).group(1)
-            NS= assetName+str(num)
+            num = re.match(r'.*?\{(\d*)\}', cnPath).group(1)
+            NS = assetName + str(num)
         else:
-            NS= assetName
+            NS = assetName
         try:
-                    pm.namespace(rename = (ref.namespace,NS))
+            pm.namespace(rename=(ref.namespace, NS))
         except Exception:
-                    pass
+            pass
