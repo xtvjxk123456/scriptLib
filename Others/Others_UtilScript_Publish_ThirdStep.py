53af2c9bc7df85795853fc1ffb8d9a51648a9f81
zhangxuqiang <zhangxuqiang@antsanimation.com>
2017-01-09 09:58:49 +0800

    Fix:use api

--- Others/Others_UtilScript_Publish_ThirdStep.py
+++ Others/Others_UtilScript_Publish_ThirdStep.py
@@ -5,8 +5,8 @@ def run():
     import Publish_Anim_AutoAnimPublish
     Publish_Anim_AutoAnimPublish.run()
 
-    import pymel.core as pm
-    pm.saveFile()
+    import maya.OpenMaya as om
+    om.MFileIO.save()
 
     import Publish_Anim_Copy_AnimSourceToPublish
     Publish_Anim_Copy_AnimSourceToPublish.run()
