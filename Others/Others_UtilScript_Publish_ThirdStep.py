c10d27be674087ace18670b4cc8fe1a53ad7043c
zhangxuqiang <zhangxuqiang@antsanimation.com>
2017-01-09 13:49:42 +0800

    Fix:api limit??

--- Others/Others_UtilScript_Publish_ThirdStep.py
+++ Others/Others_UtilScript_Publish_ThirdStep.py
@@ -1,11 +1,11 @@
 # coding:utf-8
+import maya.OpenMaya as om
 
 
 def run():
     import Publish_Anim_AutoAnimPublish
     Publish_Anim_AutoAnimPublish.run()
 
-    import maya.OpenMaya as om
     om.MFileIO.save()
 
     import Publish_Anim_Copy_AnimSourceToPublish
