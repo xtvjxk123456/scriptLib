006ec7df7d26fb2e66c563a0075af2da520af84f
zhangxuqiang <zhangxuqiang@antsanimation.com>
2017-01-21 16:55:46 +0800

    Add:完成导入相机导出相机的功能

--- Lighting/Lighting_CamDepth_ImportFocusCam.py
+++ Lighting/Lighting_CamDepth_ImportFocusCam.py
@@ -2,7 +2,8 @@
 import pymel.core as pm
 import pixoLibs.pixoFileTools as pft
 import os
-
+import previewShaderInAnim as ps
+import glob
 
 def get_latest_camera():
     try:
@@ -11,16 +12,19 @@ def get_latest_camera():
         pm.warning(u'场景文件名不正确')
         return None
     path_obj.task = 'cam'
-    publishcamfile = path_obj.getPublishFullPath()
-    mafile = os.path.splitext(publishcamfile)[0]+'.ma'
-    mbfile = os.path.splitext(publishcamfile)[0]+'.ma'
-    if os.path.exists(mbfile):
-        return mbfile
+    publishdir = os.path.dirname(os.path.dirname(path_obj.getPublishFullPath()))
+    versions = ps.find_folders_in_dir(publishdir)
+    if not versions:
+        pm.warning(u'cam没有publish过任何文件')
+        return None
+    versions.sort()
+    rightPath = os.path.join(publishdir, '{}/df_{}_{}_{}_{}_???.m?'.format(versions[-1], path_obj.seq, path_obj.shot, 'cam',versions[-1]))
+    file_path = glob.glob(os.path.normpath(rightPath))
+    if file_path:
+        return file_path[0]
     else:
-        if os.path.exists(mafile):
-            return mafile
-        else:
-            return None
+        pm.warning(u'cam文件没有publish完成?')
+        return None
 
 
 def run():
--- Lighting/Lighting_CamDepth_Publish_CamDepthFile.py
+++ Lighting/Lighting_CamDepth_Publish_CamDepthFile.py
@@ -3,6 +3,7 @@ import pymel.core as pm
 import os
 import pixoLibs.pixoFileTools as pft
 
+
 path_obj = pft.PathDetails.parse_path(pm.sceneName())
 seq = path_obj.seq
 shot = path_obj.shot
@@ -10,4 +11,10 @@ publishdir = os.path.dirname(path_obj.getPublishFullPath())
 
 
 def run():
-    return
+    pm.select(pm.ls('CamFocus:Camera'))
+    if pm.ls(type='unknown'):
+        pm.delete(pm.ls(type='unknown'))
+    publishFocus = os.path.join(publishdir, 'CamFocus/{}_{}_{}_cam.ma'.format(path_obj.project, path_obj.seq, path_obj.shot))
+    os.makedirs(os.path.dirname(os.path.normpath(publishFocus)))
+    pm.exportSelected(os.path.normpath(publishFocus),force=True)
+    pm.warning(u'导出CamFocus相机完成')
