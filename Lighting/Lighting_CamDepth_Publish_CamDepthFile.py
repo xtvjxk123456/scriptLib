4b275b82f66c35c9a3dbee062feb6269d923ccd9
zhangxuqiang <zhangxuqiang@antsanimation.com>
2017-01-21 17:44:28 +0800

    Fix:修改输出位置

--- Lighting/Lighting_CamDepth_Publish_CamDepthFile.py
+++ Lighting/Lighting_CamDepth_Publish_CamDepthFile.py
@@ -4,20 +4,20 @@ import os
 import pixoLibs.pixoFileTools as pft
 
 
-path_obj = pft.PathDetails.parse_path(pm.sceneName())
-seq = path_obj.seq
-shot = path_obj.shot
-publishdir = os.path.dirname(path_obj.getPublishFullPath())
-
-
 def run():
+    if not pm.ls('CamFocus:Camera'):
+        pm.warning(u'没有可以导出的相机')
+        return
+
     pm.select(pm.ls('CamFocus:Camera'))
     if pm.ls(type='unknown'):
         pm.delete(pm.ls(type='unknown'))
-    publishFocus = os.path.join(publishdir, 'CamFocus/{}_{}_{}_cam.ma'.format(path_obj.project, path_obj.seq, path_obj.shot))
-    os.makedirs(os.path.dirname(os.path.normpath(publishFocus)))
-    pm.exportSelected(os.path.normpath(publishFocus),force=True)
 
-    pm.saveAs(os.path.join(os.path.dirname(os.path.normpath(publishFocus)),os.path.basename(pm.sceneName())))
+    path_obj = pft.PathDetails.parse_path(pm.sceneName())
+    pubfile = path_obj.getNewVersionForTask('cam-focus', publish=True)
+    os.makedirs(os.path.dirname(pubfile))
+
+    pm.exportSelected(pubfile, force=True)
+    pm.saveAs(os.path.join(os.path.dirname(pubfile), os.path.basename(pm.sceneName())))
 
     pm.warning(u'导出CamFocus相机完成,文件另存为完成')
