9b5985f175ec9b28465db09b72f21da814271e42
zhangxuqiang <zhangxuqiang@antsanimation.com>
2017-01-22 09:41:56 +0800

    Fix:修改下代码v2

--- Lighting/Lighting_CamDepth_Publish_CamDepthFile.py
+++ Lighting/Lighting_CamDepth_Publish_CamDepthFile.py
@@ -6,7 +6,7 @@ import shutil
 
 
 def run():
-    if not pm.ls('CamFocus:Camera'):
+    if not pm.ls('cam_focus_*'):
         pm.warning(u'没有可以导出的相机')
         return
 
@@ -15,7 +15,7 @@ def run():
 
     path_obj = pft.PathDetails.parse_path(pm.sceneName())
     pubfile = path_obj.getNewVersionForTask('cam-focus', publish=True)
-    if not pubfile:
+    if not path_obj.user:
         pm.warning(u'请检查本场景文件的路径,特别注意下文件名本身')
         return
     os.makedirs(os.path.dirname(pubfile))
@@ -26,8 +26,11 @@ def run():
     # pm.sysFile(imagedir, copy=os.path.join(os.path.dirname(pubfile), 'images'))
     shutil.copytree(imagedir, os.path.join(os.path.dirname(pubfile), 'images'))
 
-    pm.select(pm.ls('CamFocus:Camera'), r=True)
+    # pm.select(pm.ls('CamFocus:Camera'), r=True)
+    cams =map(lambda x: x.getParent(), filter(lambda x: x.name().startswith('cam_focus_'), pm.ls(type='camera')))
+    pm.select(cams)
     pm.exportSelected(os.path.splitext(pubfile)[0] + '.ma', force=True)
     pm.saveAs(os.path.join(os.path.dirname(pubfile), os.path.basename(pm.sceneName())))
 
-    pm.warning(u'导出CamFocus相机完成,文件另存为完成')
+    pm.warning(u'导出CamFocus相机完成,文件另存为完成,序列帧复制完成')
+    os.startfile(os.path.join(os.path.dirname(pubfile), 'images'))
