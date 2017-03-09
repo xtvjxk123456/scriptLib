b85e2e42dc97e1063efaa971ad368cb9494721cc
zhangxuqiang <zhangxuqiang@antsanimation.com>
2017-03-09 14:20:43 +0800

    Fix:先单独删除历史,使用api来处理切分操作,使用命令来分离模型,没有构建历史,初始版本,暂未发现bug

--- Publish/Publish_Mdl_Fix_FaceShader.py
+++ Publish/Publish_Mdl_Fix_FaceShader.py
@@ -2,6 +2,7 @@
 import pymel.core as pm
 import pixoLibs.magiclist.maya_magic_list as magic_list
 import Publish_Mdl_Check_FaceShader as pmcfs
+import maya.OpenMaya as om
 
 
 def getObj():
@@ -12,18 +13,54 @@ def getObj():
 
 def fixFaceShaderObject(items):
     for item in items():
-        sgs = pm.listConnections(pm.PyNode(item), t='shadingEngine')
+        itemnode = pm.PyNode(item)
+        sgs = pm.listConnections(itemnode, t='shadingEngine')
         sgs = list(set(sgs))
         # sgs代表有多个shadingGroup
         # pm.select(shape,r=True)
+        # ------------------------------------------------------------------------------
         # hilite是醒目的意思,比较适合选择用来建模(这种选择可以方便的进入组件(点线面))
-        pm.hilite(item, r=True)
-        pm.selectMode(component=True)
+        # pm.hilite(item, r=True)
+        # pm.selectMode(component=True)
+        # ------------------------------------------------------------------------------
+        # 先删除历史
+        pm.delete(itemnode, ch=True)
+
+        # ------------------------------------------------------------------------------
         # 这里以前有个bug,这个bug只是考虑到用材质球选择面,而不是考虑到用材质球选择当前物体的面
-        pm.select([x for x in sgs[0].members() if item in x.name()], r=True)
-        pm.runtime.ExtractFace()
-        pm.runtime.DeleteHistory()
-        shapes = pm.listRelatives(pm.ls(sl=True))
+        # pm.select([x for x in sgs[0].members() if item in x.name()], r=True)
+        # ------------------------------------------------------------------------------
+        faces = [x for x in sgs[0].members() if item in x.name()]
+        faceidlist = []
+        for x in faces:
+            shellid = om.MIntArray()
+            x.__apicomponent__().getElements(shellid)
+            faceidlist.extend(list(shellid))
+        selectface = om.MIntArray()
+        om.MScriptUtil.createIntArrayFromList(faceidlist, selectface)
+
+        # ------------split face------------------------------
+
+        # trans = om.MFloatVector()
+        # meshfn = om.MFnMesh(selectobj)
+        # result = meshfn.extractFaces(faceid, trans)
+        # print result
+        # meshfn.updateSurface()
+        trans = om.MFloatVector()
+        itemnode.__apimfn__().extractFaces(selectface, trans)
+        itemnode.__apimfn__().updateSurface()
+
+        # -------------------------------------------------
+        # ------------------seprate mesh--------------------
+        # pm.runtime.ExtractFace()
+        # pm.runtime.DeleteHistory()
+        # shapes = pm.listRelatives(pm.ls(sl=True))
+
+        try:
+            shapes = pm.polySeparate(itemnode, ch=False)
+        except RuntimeError:
+            shapes = [itemnode]
+
         if filter(pmcfs.checkShaderMulti, shapes):
             for x in filter(pmcfs.checkShaderMulti, shapes):
                 fixFaceShaderObject(x)
