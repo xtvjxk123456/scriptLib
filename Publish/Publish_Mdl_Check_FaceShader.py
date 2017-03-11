9fdca8eea61271a4235fc99c62a0699c6efee0ee
zhangxuqiang <zhangxuqiang@antsanimation.com>
2017-03-11 17:04:27 +0800

    Fix:修复核心fixfaceshade命令为api,

--- Publish/Publish_Mdl_Check_FaceShader.py
+++ Publish/Publish_Mdl_Check_FaceShader.py
@@ -1,31 +1,50 @@
 # coding:utf-8
-import pymel.core as pm
+import maya.cmds as mc
+import maya.OpenMaya as om
 
 
-def checkShaderMulti(shape):
-    sgs = pm.listConnections(shape, t='shadingEngine')
-    sgs = list(set(sgs))
-    if len(sgs) > 1:
-        # print shape, 'has multi shader'
-        return True
-    if len(sgs) == 1:
-        # print shape, 'has single shader'
-        return False
-    if len(sgs) == 0:
-        # print shape, 'has no shader '
-        return False
+def checkShaderMulti(shapename):
+    if not shapename:
+        templist = om.MSelectionList()
+        om.MGlobal.getSelectionListByName(shapename, templist)
+        shapedagpath = om.MDagPath()
+        templist.getDagPath(0, shapedagpath)
+        fnmesh = om.MFnMesh(shapedagpath)
+        sgs = om.MObjectArray()
+        faceinsg = om.MObjectArray()
+        # 确保这个shape不是一个instanced对象
+        fnmesh.getConnectedSetsAndMembers(0, sgs, faceinsg, True)
+        if sgs.length() <= 1:
+            return False
+        else:
+            return True
+    else:
+        return None
 
 
 def getObjWithFaceShader():
-    allmesh = pm.ls(type='mesh')
-
-    multis = filter(checkShaderMulti, allmesh)
-    return multis
+    iter_mesh = om.MItDag(om.MItDag.kDepthFirst, om.MFn.kMesh)
+    objs = []
+    instanced = []
+    while not iter_mesh.isDone():
+        m_mesh = iter_mesh.currentItem()
+        fn_mesh = om.MFnMesh(m_mesh)
+        sgs = om.MObjectArray()
+        faceinsg = om.MObjectArray()
+        if not iter_mesh.isInstanced(False):
+            fn_mesh.getConnectedSetsAndMembers(0, sgs, faceinsg, True)
+            if sgs.length() > 1:
+                objs.append(fn_mesh.fullPathName())
+        else:
+            instanced.append(fn_mesh.fullPathName())
+            iter_mesh.next()
+        iter_mesh.next()
+    return objs, instanced
 
 
 def run():
-    meshs = getObjWithFaceShader()
-    trans = map(lambda x: x.getParent(), meshs)
-    objname = '\n'.join(map(lambda x:x.name(),trans))
+    meshs = getObjWithFaceShader()[0]
+    trans = map(lambda x: mc.listRelatives(x,p=True)[0], meshs)
+    objname = '\n'.join(trans)
     message = 'Result:\n\n' +objname
-    pm.confirmDialog(t='FaceShader Result',m = message,b='I got it!',icon ='warning')
+    mc.confirmDialog(t='FaceShader Result',m = message,b='I got it!',icon ='warning')
--- Publish/Publish_Mdl_Fix_FaceShader.py
+++ Publish/Publish_Mdl_Fix_FaceShader.py
@@ -1,70 +1,43 @@
 # coding:utf-8
-import pymel.core as pm
+import maya.cmds as mc
 import pixoLibs.magiclist.maya_magic_list as magic_list
 import Publish_Mdl_Check_FaceShader as pmcfs
 import maya.OpenMaya as om
 
 
-def getObj():
-    meshs = pmcfs.getObjWithFaceShader()
-    names = map(lambda x: x.name(), meshs)
-    return names
-
-
 def fixFaceShaderObject(items):
-    for item in items():
-        itemnode = pm.PyNode(item)
-        sgs = pm.listConnections(itemnode, t='shadingEngine')
-        sgs = list(set(sgs))
-        # sgs代表有多个shadingGroup
-        # pm.select(shape,r=True)
-        # ------------------------------------------------------------------------------
-        # hilite是醒目的意思,比较适合选择用来建模(这种选择可以方便的进入组件(点线面))
-        # pm.hilite(item, r=True)
-        # pm.selectMode(component=True)
-        # ------------------------------------------------------------------------------
-        # 先删除历史
-        pm.delete(itemnode, ch=True)
+    for shapename in items():
 
-        # ------------------------------------------------------------------------------
-        # 这里以前有个bug,这个bug只是考虑到用材质球选择面,而不是考虑到用材质球选择当前物体的面
-        # pm.select([x for x in sgs[0].members() if item in x.name()], r=True)
-        # ------------------------------------------------------------------------------
-        faces = [x for x in sgs[0].members() if item in x.name()]
-        faceidlist = []
-        for x in faces:
-            shellid = om.MIntArray()
-            x.__apicomponent__().getElements(shellid)
-            faceidlist.extend(list(shellid))
-        selectface = om.MIntArray()
-        om.MScriptUtil.createIntArrayFromList(faceidlist, selectface)
+        templist = om.MSelectionList()
+        om.MGlobal.getSelectionListByName(shapename, templist)
+        shapedagpath = om.MDagPath()
+        templist.getDagPath(0, shapedagpath)
+        fnmesh = om.MFnMesh(shapedagpath)
 
-        # ------------split face------------------------------
+        sgs = om.MObjectArray()
+        faceinsg = om.MObjectArray()
+        fnmesh.getConnectedSetsAndMembers(0, sgs, faceinsg, True)
 
-        # trans = om.MFloatVector()
-        # meshfn = om.MFnMesh(selectobj)
-        # result = meshfn.extractFaces(faceid, trans)
-        # print result
-        # meshfn.updateSurface()
-        trans = om.MFloatVector()
-        itemnode.__apimfn__().extractFaces(selectface, trans)
-        itemnode.__apimfn__().updateSurface()
+        fn_firstsg = om.MFnDependencyNode(sgs[0])
+        firstsgname = fn_firstsg.name()
 
-        # -------------------------------------------------
-        # ------------------seprate mesh--------------------
-        # pm.runtime.ExtractFace()
-        # pm.runtime.DeleteHistory()
-        # shapes = pm.listRelatives(pm.ls(sl=True))
+        fn_compent = om.MFnSingleIndexedComponent(faceinsg[0])
+        m_faceid = om.MIntArray()
+        fn_compent.getElements(m_faceid)
 
+        tempTrans = om.MFloatVector()
+        fnmesh.extractFaces(m_faceid, tempTrans)
+        fnmesh.updateSurface()
         try:
-            shapes = pm.polySeparate(itemnode, ch=False)
+            shapes = mc.polySeparate(fnmesh.fullPathName(), ch=False)
         except RuntimeError:
-            shapes = [itemnode]
+            shapes = []
 
         if filter(pmcfs.checkShaderMulti, shapes):
-            for x in filter(pmcfs.checkShaderMulti, shapes):
-                fixFaceShaderObject(x)
+            for objname in filter(pmcfs.checkShaderMulti, shapes):
+                fixFaceShaderObject(objname)
 
 
 def run():
+    getObj = lambda: pmcfs.getObjWithFaceShader()[0]
     magic_list.run(getObj, 'FaceShader Fix', {'Fix': fixFaceShaderObject})
