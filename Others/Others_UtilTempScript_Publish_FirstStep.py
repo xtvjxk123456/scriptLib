b9095b25d7edeb125841d3df163af1d1a9acf409
zhangxuqiang <zhangxuqiang@antsanimation.com>
2017-04-15 21:31:01 +0800

    Fix:添加两个临时脚本应付工作_fixbug

--- Others/Others_UtilTempScript_Publish_FirstStep.py
+++ Others/Others_UtilTempScript_Publish_FirstStep.py
@@ -2,11 +2,11 @@
 
 
 def run():
-    import pixoMaya.shelf.playblast as pb
-    pb.run()
-
     import Others_UtilTempScript_Export_AnimABC_noUI_CurrentFrame
     Others_UtilTempScript_Export_AnimABC_noUI_CurrentFrame.run()
+
+    import pixoMaya.shelf.playblast as pb
+    pb.run()
 
     # ------------------------------------------
     import Publish_Cam_CamPublish
